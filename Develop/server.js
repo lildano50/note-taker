b0dc6af4424d03313b2d18c503c81fdea01e41fd
Daniel Park <danielapark87@gmail.com>
2023-10-20 10:25:29 -0500

    added api/notes get and post

--- Develop/server.js
+++ Develop/server.js
@@ -9,6 +9,29 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/assets/js/index.js'))
 );
 
+app.get('/api/notes', (req, res) => 
+  res.sendFile(path.join(__dirname, '/db/db.json'))  
+);
+
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to submit feedback`);
+    const { title, text } = req.body;
+    if (title && text) {
+    const newNote = {
+        title,
+        text
+    };
+    readAndAppend(newNote, './db/feedback.json');
+    const response = {
+        status: 'success',
+        body: newNote,
+    };
+    res.json(response);
+    } else {
+    res.json('Error in posting feedback');
+    }
+});
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
